9e00b33a81a65ecebacea69c2dd092fd06e50987
Michael Pope <amorphid@captaincoder.io>
2014-02-12 15:19:18 -0800

    Write failing test:  Signing up with email displays success message

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+feature "Signing up" do
+  background { visit sign_up_path }
+
+  context "with email" do
+    scenario "displays success message" do
+      body = page.body
+      expect(body).to have_content("Sign up successful")
+    end
+
+    scenario "creates a SignUp"
+  end
+
+  context "without email" do
+    scenario "displays error message"
+  end
+end
