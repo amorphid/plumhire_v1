f1a72284dab6e99ebe66e7c84703ef543ba271e5
Michael Pope <amorphid@captaincoder.io>
2014-02-12 16:05:14 -0800

    Fix typo in SignUp model test

--- spec/models/sign_up_spec.rb
+++ spec/models/sign_up_spec.rb
@@ -4,9 +4,10 @@ describe SignUp do
   subject { Fabricate.build(:sign_up) }
 
   context "validates presence of" do
-    it "email"
+    it "email" do
       expect(subject).to have(0).errors_on(:email)
       subject.first_name = nil
       expect(subject).to have(1).errors_on(:email)
+    end
   end
 end
