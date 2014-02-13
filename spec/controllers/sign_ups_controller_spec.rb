6b62e0a59cba4899bdaa38aae7af839c21d2624b
Michael Pope <amorphid@captaincoder.io>
2014-02-12 16:28:59 -0800

    Refactor failing test => SignUpsController#new sets @sign_up

--- spec/controllers/sign_ups_controller_spec.rb
+++ spec/controllers/sign_ups_controller_spec.rb
@@ -1,11 +1,9 @@
 require "spec_helper"
 
 describe SignUpsController do
-  subject { Fabricate(:sign_up) }
-
   context "#new" do
     it "sets @sign_up" do
-      sign_up = subject
+      sign_up = SignUp.new
       get :new
       assignment = assigns(:sign_up)
       expect(assignment).to eq(sign_up)
