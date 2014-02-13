7a1ffe3f8e2abe87a9457fee51ecc3225e644854
Michael Pope <amorphid@captaincoder.io>
2014-02-12 16:39:03 -0800

    Test passing => SignUpsController#new sets @sign_up

--- spec/controllers/sign_ups_controller_spec.rb
+++ spec/controllers/sign_ups_controller_spec.rb
@@ -3,10 +3,9 @@ require "spec_helper"
 describe SignUpsController do
   context "#new" do
     it "sets @sign_up" do
-      sign_up = SignUp.new
       get :new
       assignment = assigns(:sign_up)
-      expect(assignment).to eq(sign_up)
+      expect(assignment).to be_instance_of(SignUp)
     end
   end
 end
