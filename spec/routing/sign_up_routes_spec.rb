fae425fe720d2f5d3966f019e4c9f6e7d2b410da
Michael Pope <amorphid@captaincoder.io>
2014-02-14 10:00:03 -0800

    Refactor routing test for new_sign_up_path

--- spec/routing/sign_up_routes_spec.rb
+++ spec/routing/sign_up_routes_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
 
-describe "/sign_ups" do
-  it "routes to SignUpsController#index" do
-    expect(get: "/sign_ups").to route_to( controller: "sign_ups",
-                                              action: "index" )
+describe "Route" do
+  context "sign_ups_path" do
+    it "routes to SignUpsController#index" do
+      expect(get: "/sign_ups").to route_to(
+        controller: "sign_ups",
+        action: "index"
+      )
+    end
   end
 end
