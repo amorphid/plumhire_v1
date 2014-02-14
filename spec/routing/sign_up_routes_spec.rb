03ca02884c526f76fdc4ca430592f9b2687cfb9c
Michael Pope <amorphid@captaincoder.io>
2014-02-13 16:38:57 -0800

    Break test for new sign up route => /sign_ups routes to SignUpsController#index
    - Test failing
    - In routes.rb, need to update resources :sign_ups => only: [:new] becomes
      only: [:index]

--- spec/routing/sign_up_routes_spec.rb
+++ spec/routing/sign_up_routes_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
-describe "/sign_ups/new" do
-  it "routes to SignUpsController#new" do
-    expect(get: "/sign_ups/new").to route_to( controller: "sign_ups",
-                                              action: "new" )
+describe "/sign_ups" do
+  it "routes to SignUpsController#index" do
+    expect(get: "/sign_ups").to route_to( controller: "sign_ups",
+                                              action: "index" )
   end
 end
