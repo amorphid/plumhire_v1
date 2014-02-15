38d09e4d17c663a224cf34ba0797048ab6fd2cde
Michael Pope <amorphid@captaincoder.io>
2014-02-14 19:47:35 -0800

    Set SignUpsController#index to use ModelDecorator on SignUp

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,5 +1,7 @@
 class SignUpsController < ApplicationController
   def index
-    @sign_up = SignUp.new
+    sign_up   = SignUp.new
+    decorator = ModelDecorator.new(sign_up)
+    @sign_up  = decorator.response
   end
 end
