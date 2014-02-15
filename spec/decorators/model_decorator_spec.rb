55f1db04d6db64e78cbd8dee480dccf6a318620f
Michael Pope <amorphid@captaincoder.io>
2014-02-14 18:40:24 -0800

    Red:  ModelDecorator#decorate adds a uuid to a model
    -----FAILING TEST-----
    
    1) ModelDecorator#decorate adds a uuid to a model
       Failure/Error: uuid               = ModelDecorator.new.decorate(model_without_uuid)
       ArgumentError:
         wrong number of arguments (0 for 1)

--- spec/decorators/model_decorator_spec.rb
+++ spec/decorators/model_decorator_spec.rb
@@ -9,6 +9,14 @@ describe ModelDecorator do
 
   subject { ModelDecorator.new(model) }
 
+  context "#decorate" do
+    it "adds a uuid to a model" do
+      model_without_uuid = model.new(uuid: nil)
+      uuid               = ModelDecorator.new.decorate(model_without_uuid)
+      expect(uuid).to_match(version_5_uuid)
+    end
+  end
+
   context "#uuid" do
     it "generates a Version 5 (SHA-1 hash) UUID" do
       uuid = subject.uuid
