32955314094a1065f88a89003ee6f78e5fc12721
Michael Pope <amorphid@captaincoder.io>
2014-02-15 15:30:54 -0800

    Write failing test => Version5UuidSetter#object returns an object with a Version 5 UUID
    -----FAILING TEST-----
    
    2) Version5UuidSetter#object returns an object with a Version 5 UUID
       Failure/Error: uuid   = object.uuid
       NoMethodError:
         undefined method `uuid' for nil:NilClass

--- spec/setters/version_5_uuid_setter_spec.rb
+++ spec/setters/version_5_uuid_setter_spec.rb
@@ -1,19 +1,27 @@
 require "spec_helper"
 
 describe Version5UuidSetter do
+  let(:version_5_uuid_pattern) do
+    /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
+  end
+
   subject do
     SomeObject  = Struct.new(:uuid)
     some_object = SomeObject.new
     Version5UuidSetter.new(some_object)
   end
 
+  context "#object" do
+    it "returns an object with a Version 5 UUID" do
+      object = subject.object
+      uuid   = object.uuid
+      expect(uuid).to match(version_5_uuid_pattern)
+    end
+  end
+
   context "#uuid" do
     it "returns a Version 5 UUID" do
       uuid = subject.uuid
-
-      version_5_uuid_pattern =
-        /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
-
       expect(uuid).to match(version_5_uuid_pattern)
     end
   end
