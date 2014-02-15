8e1e6f74ab3e4bb5030ef7ae2d9e887bf0c4a150
Michael Pope <amorphid@captaincoder.io>
2014-02-14 18:23:04 -0800

    Green:  ModelDecorator#uuid adds a Version 5 (SHA-1 hash) UUID
    - Test passing.  Woohoo!

--- app/decorators/model_decorator.rb
+++ app/decorators/model_decorator.rb
@@ -8,4 +8,11 @@ class ModelDecorator
   def response
     model
   end
+
+  def uuid
+    timestamp   = UUIDTools::UUID.timestamp_create
+    mac_address = UUIDTools::UUID.mac_address
+    uuid        = UUIDTools::UUID.sha1_create(timestamp, mac_address)
+    uuid.to_s
+  end
 end
