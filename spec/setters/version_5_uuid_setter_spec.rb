b83bec73e31ffb95ddec1f2f963932a767e1725a
Michael Pope <amorphid@captaincoder.io>
2014-02-15 15:20:13 -0800

    Refactor test for Version5UuidSetter#uuid

--- spec/setters/version_5_uuid_setter_spec.rb
+++ spec/setters/version_5_uuid_setter_spec.rb
@@ -2,16 +2,19 @@ require "spec_helper"
 
 describe Version5UuidSetter do
   subject do
-    SomeObject = Struct.new(:uuid)
-    SomeObject.new
+    SomeObject  = Struct.new(:uuid)
+    some_object = SomeObject.new
+    Version5UuidSetter.new(some_object)
   end
 
-  before { Version5UuidSetter.new(subject) }
+  context "#uuid" do
+    it "returns a Version 5 UUID" do
+      uuid = subject.uuid
 
-  it "sets SomeObject#uuid to Version 5 UUID" do
-    uuid                   = subject.uuid
-    version_5_uuid_pattern =
-      /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
-    expect(uuid).to match(version_5_uuid_pattern)
+      version_5_uuid_pattern =
+        /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
+
+      expect(uuid).to match(version_5_uuid_pattern)
+    end
   end
 end
