f730729f0198b8893976766985c7bcf8c25d5c0b
Michael Pope <amorphid@captaincoder.io>
2014-02-15 17:20:16 -0800

    Delete unneeded methods from ModelDecorator class

--- app/decorators/model_decorator.rb
+++ app/decorators/model_decorator.rb
@@ -1,25 +1,2 @@
 class ModelDecorator
-  attr_reader :model
-
-  def initialize(model)
-    @model = model
-  end
-
-  def override_to_param_method
-    to_param = "def to_param; \"uuid\"; end"
-    model.class.module_eval(to_param)
-  end
-
-  def response
-    override_to_param_method
-    model.uuid = uuid
-    model
-  end
-
-  def uuid
-    timestamp   = UUIDTools::UUID.timestamp_create
-    mac_address = UUIDTools::UUID.mac_address
-    uuid        = UUIDTools::UUID.sha1_create(timestamp, mac_address)
-    uuid.to_s
-  end
 end
