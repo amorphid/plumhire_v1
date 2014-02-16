78973c827e1d73c3536c1cedd35c0781304579a4
Michael Pope <amorphid@captaincoder.io>
2014-02-16 12:52:49 -0800

    (green) Make test pass => Version5UuidConstructor#mac_address retrieves computers mac address

--- app/constructors/version5_uuid_constructor.rb
+++ app/constructors/version5_uuid_constructor.rb
@@ -1,4 +1,8 @@
 class Version5UuidConstructor
+  def mac_address
+    UUIDTools::UUID.mac_address
+  end
+
   def response
     # this generates a UUID, not it's not Version 5
     # Version 5 uses a SHA-1 encryption hash
