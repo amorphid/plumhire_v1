710453be4e0cebcccc87cc8203dd2c592067e00d
Michael Pope <amorphid@captaincoder.io>
2014-02-16 14:13:21 -0800

    Delete all code within Version5UuidSetter class

--- app/setters/version5_uuid_setter.rb
+++ app/setters/version5_uuid_setter.rb
@@ -1,15 +1,2 @@
 class Version5UuidSetter
-  attr_reader :object
-
-  def initialize(object)
-    @object     = object
-    object.uuid = uuid
-  end
-
-  def uuid
-    timestamp   = UUIDTools::UUID.timestamp_create
-    mac_address = UUIDTools::UUID.mac_address
-    uuid        = UUIDTools::UUID.sha1_create(timestamp, mac_address)
-    uuid.to_s
-  end
 end
