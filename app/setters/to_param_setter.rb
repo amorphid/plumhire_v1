549a81b5f35be365455c9b59556f29f8fe229834
Michael Pope <amorphid@captaincoder.io>
2014-02-15 17:00:45 -0800

    (green) Make test pass => ToParamSetter#override_to_param returns :to_param

--- app/setters/to_param_setter.rb
+++ app/setters/to_param_setter.rb
@@ -1,4 +1,12 @@
 class ToParamSetter
+  attr_reader :object
+
   def initialize(object)
+    @object = object
+  end
+
+  def override_to_param
+    to_param  = "def to_param; \"uuid\"; end"
+    object.instance_eval(to_param)
   end
 end
