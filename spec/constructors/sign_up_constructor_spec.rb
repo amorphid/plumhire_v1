05a13d9682b476363224a09bfb81670514e7d198
Michael Pope <amorphid@captaincoder.io>
2014-02-18 14:36:23 -0800

    (red) Write failing test => SignUpConstructor#model_decorator returns an instance of ModelDecorator

--- spec/constructors/sign_up_constructor_spec.rb
+++ spec/constructors/sign_up_constructor_spec.rb
@@ -3,6 +3,14 @@ require "spec_helper"
 describe SignUpConstructor do
   subject { SignUpConstructor.new }
 
+  context "#model_decorator" do
+    it "returns an instance of ModelDecorator" do
+      object   = SRO::ReaderWriter.new(uuid: nil)
+      instance = subject.model_decorator(object)
+      expect(instance).to be_instance_of(ModelDecorator)
+    end
+  end
+
   context "#response" do
     it "returns a SignUp instance" do
       response = subject.response
