b71da161830cb3ba7315226730cbef75b6568842
Michael Pope <amorphid@captaincoder.io>
2014-02-17 21:17:43 -0800

    (green) Write failing test => Version5UuidSetter#set_uuid sets uuid to object if object uuid is blank

--- spec/setters/version5_uuid_setter_spec.rb
+++ spec/setters/version5_uuid_setter_spec.rb
@@ -3,6 +3,10 @@ require "spec_helper"
 describe Version5UuidSetter do
   subject { Version5UuidSetter.new }
 
+  let(:uuid_pattern) do
+    /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
+  end
+
   context "#object_has_uuid?" do
     it "returns true with uuid" do
       uuid          = Version5UuidConstructor.new.response
@@ -19,6 +23,13 @@ describe Version5UuidSetter do
   end
 
   context "#set_uuid" do
+    it "sets uuid to object if object uuid is blank" do
+      object   = SRO::ReaderWriter.new(uuid: nil)
+      subject.set_uuid(object)
+      uuid     = object.uuid
+      expect(uuid).to match(uuid_pattern)
+    end
+
     it "does not set uuid if object is not blank" do
       uuid1  = Version5UuidConstructor.new.response
       object = SRO::ReaderWriter.new(uuid: uuid1)
