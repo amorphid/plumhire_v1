4a9b192b6d2e6a13d1f19f0c42bad567aeb165e0
Michael Pope <amorphid@captaincoder.io>
2014-02-17 18:20:52 -0800

    (red) Write failing test => Version5UuidSetter#object_has_uuid? returns false without uuid

--- spec/setters/version_5_uuid_setter_spec.rb
+++ spec/setters/version_5_uuid_setter_spec.rb
@@ -1,28 +1,13 @@
 require "spec_helper"
 
 describe Version5UuidSetter do
-  let(:version_5_uuid_pattern) do
-    /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
-  end
+  subject { Version5UuidSetter.new }
 
-  subject do
-    SomeObject  = Struct.new(:uuid)
-    some_object = SomeObject.new
-    Version5UuidSetter.new(some_object)
-  end
-
-  context "#object" do
-    it "returns an object with a Version 5 UUID" do
-      object = subject.object
-      uuid   = object.uuid
-      expect(uuid).to match(version_5_uuid_pattern)
-    end
-  end
-
-  context "#uuid" do
-    it "returns a Version 5 UUID" do
-      uuid = subject.uuid
-      expect(uuid).to match(version_5_uuid_pattern)
+  context "#object_has_uuid?" do
+    it "returns false without uuid" do
+      object = NGR::ReaderWriter.new(uuid: nil)
+      true_or_false = subject.object_has_uuid?(object)
+      expect(true_or_false).to be_false
     end
   end
 end
