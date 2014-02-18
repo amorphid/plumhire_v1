cfbd95a97d1d47d818c9d77a58c9b376e36e5724
Michael Pope <amorphid@captaincoder.io>
2014-02-17 21:44:10 -0800

    (red) Refactoring tests for ToParamInjector class => ToParamInjector#inject_to_param overrides to_param method

--- spec/injectors/to_param_injector_spec.rb
+++ spec/injectors/to_param_injector_spec.rb
@@ -1,24 +1,14 @@
 require "spec_helper"
 
-describe ToParamSetter do
-  subject do
-    SomeObject  = Struct.new(:to_param)
-    some_object = SomeObject.new
-    ToParamSetter.new(some_object)
-  end
+describe ToParamInjector do
+  subject { ToParamInjector.new }
 
-  context "#response" do
-    it "returns object with to_param equal to 'uuid'" do
-      object   = subject.response
+  context "#inject_to_param" do
+    it "overrides to_param method" do
+      object   = SRO::ReaderWriter.new(to_param: "not uuid")
+      subject.inject_to_param(object)
       to_param = object.to_param
       expect(to_param).to eq("uuid")
-    end
-  end
-
-  context "#override_to_param" do
-    it "returns :to_param" do
-      override = subject.override_to_param
-      expect(override).to eq(:to_param)
     end
   end
 end
