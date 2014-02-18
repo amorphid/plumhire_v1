857590c120da4ddd019c3d611853c16b5e33ccaf
Michael Pope <amorphid@captaincoder.io>
2014-02-18 10:44:06 -0800

    (red) Write failing testModelDecorator#to_param_injector returns an instance of ToParamInjector

--- spec/decorators/model_decorator_spec.rb
+++ spec/decorators/model_decorator_spec.rb
@@ -1,4 +1,14 @@
 require "spec_helper"
 
 describe ModelDecorator do
+  subject { ModelDecorator.new }
+
+  context "#to_param_injector" do
+    it "returns an instance of ToParamInjector" do
+      model    = SRO::ReaderWriter.new
+      instance = subject.to_param_injector(model)
+      exepct(instance).to be_instance_of(ToParamInjector)
+    end
+  end
 end
+
