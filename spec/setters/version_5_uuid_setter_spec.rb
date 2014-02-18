8aae7d6f47e86fa20541e362534de348a5c64bb8
Michael Pope <amorphid@captaincoder.io>
2014-02-17 18:51:23 -0800

    (red) Version5UuidSetter#object_has_uuid? returns true with uuid

--- spec/setters/version_5_uuid_setter_spec.rb
+++ spec/setters/version_5_uuid_setter_spec.rb
@@ -4,8 +4,15 @@ describe Version5UuidSetter do
   subject { Version5UuidSetter.new }
 
   context "#object_has_uuid?" do
+    it "returns true with uuid" do
+      uuid          = Version5UuidConstructor.new.response
+      object        = NGR::ReaderWriter.new(uuid: uuid)
+      true_or_false = subject.object_has_uuid?(object)
+      expect(true_or_false).to eq(true)
+    end
+
     it "returns false without uuid" do
-      object = NGR::ReaderWriter.new(uuid: nil)
+      object        = NGR::ReaderWriter.new(uuid: nil)
       true_or_false = subject.object_has_uuid?(object)
       expect(true_or_false).to eq(false)
     end
