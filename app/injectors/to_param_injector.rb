bb1a16bab23c1ed17f526267766d8c3c6123014f
Michael Pope <amorphid@captaincoder.io>
2014-02-17 21:45:43 -0800

    (green) ToParamInjector#inject_to_param overrides to_param method

--- app/injectors/to_param_injector.rb
+++ app/injectors/to_param_injector.rb
@@ -1,16 +1,12 @@
-class ToParamSetter
+class ToParamInjector
   attr_reader :object
 
-  def initialize(object)
-    @object = object
+  def initialize(args = {})
+    @object = args[:object]
+    inject_to_param
   end
 
-  def response
-    override_to_param
-    object
-  end
-
-  def override_to_param
+  def inject_to_param(object = object)
     to_param  = "def to_param; \"uuid\"; end"
     object.instance_eval(to_param)
   end
