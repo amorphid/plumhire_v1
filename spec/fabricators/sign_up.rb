95befbe8854ecd7e4433599f0d96eeb52cf949e7
Michael Pope <amorphid@captaincoder.io>
2014-02-19 14:59:53 -0800

    Add uuid to SignUp fabricator

--- spec/fabricators/sign_up.rb
+++ spec/fabricators/sign_up.rb
@@ -1,3 +1,4 @@
 Fabricator(:sign_up) do
   email Faker::Internet.email
+  uuid SRO::Uuid.version5
 end
--- spec/support/macros/sro_uuid_pattern.rb
+++ spec/support/macros/sro_uuid_pattern.rb
@@ -3,5 +3,9 @@ module SRO
     def self.pattern
       /([a-z]|\d){8}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){4}-([a-z]|\d){12}/
     end
+
+    def self.version5
+      Version5UuidConstructor.new.response
+    end
   end
 end
