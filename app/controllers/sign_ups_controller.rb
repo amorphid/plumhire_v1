c568073f32a6af55ca54c1c35a9742716e2f2392
Michael Pope <amorphid@captaincoder.io>
2014-02-19 16:36:25 -0800

    (green) Make test pass => SignUpsController#update sets @sign_up

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -2,4 +2,15 @@ class SignUpsController < ApplicationController
   def index
     @sign_up = SignUpConstructor.new.response
   end
+
+  def update
+    @sign_up = SignUp.find_or_create_by!(params_sign_up)
+    redirect_to sign_ups_path
+  end
+
+private
+
+  def params_sign_up
+    params[:sign_up].permit(:email, :uuid)
+  end
 end
