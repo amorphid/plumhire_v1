e6a57000291c3faefe3a6bd940b3caff5ddd0069
Michael Pope <amorphid@captaincoder.io>
2014-02-23 23:49:26 -0800

    (green) Make test pass => SignUp#set_uuid should be a valid UUID

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -1,7 +1,13 @@
 class SignUp < ActiveRecord::Base
+  after_initialize :set_uuid
+
   validates :email,
             presence: true,
             uniqueness: true
 
   validates :uuid, presence: true, uniqueness: true
+
+  def set_uuid
+    self.uuid ||= Sro::Uuid.version5
+  end
 end
