81210c5993ff27120541dff37c094ecbdea5862b
Michael Pope <amorphid@captaincoder.io>
2014-02-23 23:09:43 -0800

    Refactor validation test in sign_up model to check for uuid, not email

--- spec/models/sign_up_spec.rb
+++ spec/models/sign_up_spec.rb
@@ -3,11 +3,5 @@ require "spec_helper"
 describe SignUp do
   subject { Fabricate.build(:sign_up) }
 
-  context "validates presence of" do
-    it "email" do
-      expect(subject).to have(0).errors_on(:email)
-      subject.email = nil
-      expect(subject).to have(1).errors_on(:email)
-    end
-  end
+  it { should validate_presence_of(:uuid) }
 end
