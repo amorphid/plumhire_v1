5975da24c24acfc2d405c95f32b5d526ad224a00
Michael Pope <amorphid@captaincoder.io>
2014-02-24 00:29:29 -0800

    Refactor model tests for SignUp

--- spec/models/sign_up_spec.rb
+++ spec/models/sign_up_spec.rb
@@ -1,34 +1,34 @@
 require "spec_helper"
 
 describe SignUp do
-  subject { SignUp.new }
+  let(:new_sign_up) { SignUp.new          }
+  let(:pattern)     { Sro::Uuid.pattern   }
+  let(:sign_up)     { Fabricate(:sign_up) }
 
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:uuid) }
+  it { should validate_presence_of(:uuid)  }
 
   it do
-    Fabricate(:sign_up)
+    sign_up
     should validate_uniqueness_of(:uuid)
   end
 
   context "#set_uuid" do
     it "creates UUID for new SignUp" do
-      uuid    = subject.uuid
-      pattern = Sro::Uuid.pattern
+      uuid    = new_sign_up.uuid
       expect(uuid).to match(pattern)
     end
 
     it "does not alter existing Uuid" do
-      uuid1 = subject.uuid
-      uuid2 = subject.set_uuid
+      uuid1 = new_sign_up.uuid
+      uuid2 = new_sign_up.set_uuid
       expect(uuid1).to eq(uuid2)
     end
   end
 
   context "#to_param" do
     it "returns a valid UUID" do
-      to_param = Fabricate(:sign_up).to_param
-      pattern  = Sro::Uuid.pattern
+      to_param = sign_up.to_param
       expect(to_param).to match(pattern)
     end
   end
