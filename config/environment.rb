96e077790c6bd21af583d272c6f978c94be0a075
Michael Pope <amorphid@captaincoder.io>
2014-02-25 11:14:14 -0800

    Update config/envinronment.rb w/ ActionMailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Plumhire::Application.initialize!
+
+# sending email
+ActionMailer::Base.default_url_options = { host: ENV["domain"] }
+ActionMailer::Base.smtp_settings = {
+  user_name:            ENV["sendgrid_email_username"],
+  password:             ENV["sendgrid_email_password"],
+  domain:               ENV["domain"],
+  address:              "smtp.sendgrid.net",
+  port:                 587,
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
