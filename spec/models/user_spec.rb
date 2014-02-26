ef409b787730e549d49d301677842f9014f418ca
Michael Pope <amorphid@captaincoder.io>
2014-02-26 12:24:56 -0800

    (red) Write failing test => User should require uuid to be set

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require "spec_helper"
 
 describe User do
+  #email
   it { should validate_presence_of(:email)   }
   it { should validate_uniqueness_of(:email) }
+
+  #uuid
+  it { should validate_presence_of(:uuid) }
 end
