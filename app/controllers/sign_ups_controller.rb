f3981f771bc9faa09799860e4202e29492f30be7
Michael Pope <amorphid@captaincoder.io>
2014-02-26 15:30:08 -0800

    Refactor SignUpsController code and tests

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,24 +1,24 @@
 class SignUpsController < ApplicationController
   def new
-    @sign_up = SignUp.new
+    @s = SignUp.new
   end
 
   def show
-    @sign_up = SignUp.find_by(uuid: params[:id])
+    @s = SignUp.find_by(uuid: params[:id])
   end
 
   def update
-    @sign_up = SignUp.find_or_create_by(uuid: params_sign_up[:uuid])
+    @s = SignUp.find_or_create_by(uuid: params_sign_up[:uuid])
 
-    if @sign_up.update(params_sign_up)
-      User.create(
-        email: params_sign_up[:email],
-        password: params_sign_up[:password]
-      ) unless @sign_up.user
+    if @s.update(params_sign_up)
+      # User.create(
+      #   email: params_sign_up[:email],
+      #   password: params_sign_up[:password]
+      # ) unless @s.user
 
-      send_set_password_email unless @sign_up.email_sent_on
-      flash[:notice] = "Confirmation email sent to " + @sign_up.email
-      redirect_to sign_up_path(@sign_up)
+      send_set_password_email unless @s.email_sent_on
+      flash[:notice] = "Confirmation email sent to " + @s.email
+      redirect_to sign_up_path(@s)
     else
       render action: "new"
     end
@@ -27,8 +27,8 @@ class SignUpsController < ApplicationController
 private
 
   def send_set_password_email
-    AppMailer.delay.set_password(@sign_up)
-    @sign_up.update(email_sent_on: DateTime.now)
+    AppMailer.delay.set_password(@s)
+    @s.update(email_sent_on: DateTime.now)
   end
 
   def params_sign_up
--- spec/controllers/sign_ups_controller_spec.rb
+++ spec/controllers/sign_ups_controller_spec.rb
@@ -2,84 +2,89 @@ require "spec_helper"
 
 describe SignUpsController do
   context "#new" do
-    it "sets @sign_up" do
+    it "sets @s" do
       get :new
-      assignment = assigns(:sign_up)
-      expect(assignment).to be_instance_of(SignUp)
+      expect(assigns(:s)).to be_instance_of(SignUp)
     end
   end
 
   context "#show" do
-    it "sets @sign_up" do
-      sign_up = Fabricate(:sign_up)
-      get :show, sign_up: sign_up.attributes, id: sign_up.uuid
-      assignment = assigns(:sign_up)
-      expect(assignment).to be_instance_of(SignUp)
+    it "sets @s" do
+      s = Fabricate(:sign_up)
+      get :show,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(assigns(:s)).to be_instance_of(SignUp)
     end
   end
 
   context "#update" do
     it "sets @sign_up" do
-      sign_up = Fabricate.build(:sign_up)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
-      assignment = assigns(:sign_up)
-      expect(assignment).to be_instance_of(SignUp)
+      s = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(assigns(:s)).to be_instance_of(SignUp)
     end
 
     it "creates a SignUp if it doesn't exist" do
-      pre_count    = SignUp.count
-      sign_up      = Fabricate.build(:sign_up)
-      put :update, sign_up: sign_up.attributes, id: sign_up.uuid
-      post_count   = SignUp.count
-      expect(post_count).to eq(pre_count + 1)
+      count = SignUp.count
+      s     = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(SignUp.count).to eq(count + 1)
     end
 
     it "updates a SignUp if it does exist" do
-      sign_up = Fabricate(:sign_up, email: "a@b.c")
-      sign_up.email = "1@2.3"
-      put :update, sign_up: sign_up.attributes, id: sign_up.uuid
-      email = SignUp.find_by(uuid: sign_up.uuid).email
-      expect(email).to eq("1@2.3")
+      s1 = Fabricate(:sign_up, email: "a@b.c")
+      s2 = Fabricate.build(:sign_up, email: "1@2.3", uuid: s1.uuid)
+      put :update,
+          sign_up: s2.attributes,
+          id:      s2.uuid
+      expect(SignUp.find_by(uuid: s1.uuid).email).to eq("1@2.3")
     end
 
     it "creates a User if SignUp has no User" do
-      count1 = User.count
-      sign_up = Fabricate.biuld(:sign_up)
-      put :update, sign_up: sign_up.attributes, id: sign_up.uuid
-      count2 = User.count
-      expect(count2).to eq(count1 + 1)
+      count = User.count
+      s     = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(User.count).to eq(count + 1)
     end
 
     it "sends user an email w/ valid input if email_sent_on is blank" do
-      sign_up = Fabricate.build(:sign_up)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
+      s = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
       expect(ActionMailer::Base.deliveries).not_to be_empty
     end
 
     it "sets email_sent_on when email sent" do
-      sign_up       = Fabricate.build(:sign_up)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
-      email_sent_on = SignUp.find_by(uuid: sign_up.uuid).email_sent_on
-      expect(email_sent_on).to be_instance_of(ActiveSupport::TimeWithZone)
+      s = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(SignUp.find_by(uuid: s.uuid).email_sent_on).
+        to be_instance_of(ActiveSupport::TimeWithZone)
     end
 
     it "does not send an email if email_sent_on exists" do
-      sign_up = Fabricate(:sign_up, email_sent_on: DateTime.now)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
+      s = Fabricate(:sign_up, email_sent_on: DateTime.now)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
       expect(ActionMailer::Base.deliveries).to be_empty
     end
 
-    it "creates error on @sign_up with invalid input" do
-      sign_up = Fabricate.build(:sign_up, email: nil)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
-      assignment = assigns(:sign_up)
-      expect(assignment).to have(1).errors
-    end
-
     it "redirects to sign_up_path(@sign_up) with valid input" do
-      sign_up = Fabricate.build(:sign_up)
-      put :update, id: sign_up.uuid, sign_up: sign_up.attributes
-      expect(response).to redirect_to(sign_up_path(sign_up))
+      s = Fabricate.build(:sign_up)
+      put :update,
+          sign_up: s.attributes,
+          id:      s.uuid
+      expect(response).to redirect_to(sign_up_path(s))
     end
   end
 end
