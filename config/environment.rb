5120ea2e2f28acb60152f39e412730aeb0caac80
Michael Pope <amorphid@captaincoder.io>
2014-02-25 21:52:19 -0800

    Add Redis Cloud settings to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Plumhire::Application.initialize!
 
-# sending email
+# Redis Cloud
+if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
+# SendGrid
 ActionMailer::Base.default_url_options = { host: ENV["domain"] }
 ActionMailer::Base.smtp_settings = {
   user_name:            ENV["sendgrid_email_username"],
