ecf139226c51f660315fcae029ef8019ffb39853
Michael Pope <amorphid@captaincoder.io>
2014-03-20 14:31:10 -0700

    (red) Break passing feature test => Signing up displays message

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -2,11 +2,11 @@ class AppMailer < ActionMailer::Base
   default from: "mpope.cr@gmail.com"
 
   def set_password(sign_up)
-    @sign_up = sign_up
-    @subject = "[Plumhire] Set password to access account"
+    @s       = sign_up
+    @subject = "[Plumhire] Sign Up / Step 2 of 3: Confirm email"
 
     mail(
-      to: @sign_up.email,
+      to:      @s.email,
       subject: @subject
     )
   end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,13 +1,28 @@
 require "spec_helper"
 
 feature "Signing up" do
-  background { visit new_sign_up_email_path }
-
   given(:email) { Faker::Internet.email }
 
   scenario "displays message" do
+    # /sign_up_emails/new
+    visit new_sign_up_email_path
     fill_in "sign_up_email", with: email
     click_button("Submit")
+
+    # /submit_email/:id
     expect(page.body).to have_content("Hey #{email},")
+
+    # an email
+    open_email(email)
+    current_email.click_link("click here")
+
+    # /sign_up_passwords/new
+    fill_in "sign_up_password", with: "password"
+    click_button("Submit")
+
+    # /home
+    expect(page.body).to have_content(
+      "Sign Up successful, and you're now logged in, too!"
+    )
   end
 end
