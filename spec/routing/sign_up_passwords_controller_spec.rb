d77cf598c70c96c764f3683d9c712ab87489c6a7
Michael Pope <amorphid@captaincoder.io>
2014-03-20 16:14:32 -0700

    (red) Write failing routing test => PUT /sign_up_passwords/:id

--- spec/routing/sign_up_passwords_controller_spec.rb
+++ spec/routing/sign_up_passwords_controller_spec.rb
@@ -8,4 +8,12 @@ describe SignUpPasswordsController do
       action:     "edit"
     )
   end
+
+  it do
+    should route(:put, "/sign_up_passwords/1").to(
+      controller: "sign_up_passwords",
+      id:         "1",
+      action:     "update"
+    )
+  end
 end
