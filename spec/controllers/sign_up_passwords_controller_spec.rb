61dfebd77be5a4c90b54a352c0917d7796f1beb8
Michael Pope <amorphid@captaincoder.io>
2014-03-20 17:10:39 -0700

    (red) Write failing controller test => SignUpPasswordsController#edit sets @user

--- spec/controllers/sign_up_passwords_controller_spec.rb
+++ spec/controllers/sign_up_passwords_controller_spec.rb
@@ -14,5 +14,18 @@ describe SignUpPasswordsController do
       )
       expect(assigns[:sign_up]).to be_instance_of(SignUp)
     end
+
+    it "sets @sign_up" do
+      s = Fabricate(
+        :sign_up,
+        email: Faker::Internet.email
+      )
+      get(
+        :edit,
+        sign_up: s.attributes,
+        id:      s.uuid
+      )
+      expect(assigns[:user]).to be_instance_of(User)
+    end
   end
 end
