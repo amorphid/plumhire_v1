b20fb4581647491e7f73cb7a33f3f24a9c0b1dc8
Michael Pope <amorphid@captaincoder.io>
2014-03-25 18:15:03 -0700

    (green) Make test pass => SignUpPasswordsController#update (w/ valid input) creates a user

--- app/controllers/sign_up_passwords_controller.rb
+++ app/controllers/sign_up_passwords_controller.rb
@@ -6,6 +6,15 @@ class SignUpPasswordsController < ApplicationController
 
   def update
     @sign_up = SignUp.find_by(uuid: params[:id])
-    @user    = User.new(params[:user])
+    @user    = User.find_or_initialize_by(email: sign_up_params[:email])
+    @user.password = params[:password]
+    @user.password_confirmation = params[:password]
+    @user.save
+  end
+
+  private
+
+  def sign_up_params
+    params[:sign_up].permit(:email)
   end
 end
