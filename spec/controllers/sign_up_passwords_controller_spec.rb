399492bf2992b303302dac25c2c6638c5f410205
Michael Pope <amorphid@captaincoder.io>
2014-03-25 17:21:18 -0700

    (red) Write failing test => SignUpPasswordsController#update (w/ valid input) sets @sign_up

--- spec/controllers/sign_up_passwords_controller_spec.rb
+++ spec/controllers/sign_up_passwords_controller_spec.rb
@@ -30,7 +30,16 @@ describe SignUpPasswordsController do
   end
 
   context "#update (w/ valid input)" do
-    it "sets @sign_up"
+    it "sets @sign_up" do
+      s = Fabricate(:sign_up)
+      put(
+        :update,
+        sign_up: s.attributes,
+        id:      s.uuid
+      )
+      expect(assigns[:sign_up]).to be_instance_of(SignUp)
+    end
+
     it "sets @user"
     it "redirects"
     it "creates a user"
