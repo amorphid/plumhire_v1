d316594268a43f493463f3b6b4d98f6c3b7b2051
Michael Pope <amorphid@captaincoder.io>
2014-04-07 08:08:41 -0700

    Change new job ad feature to new job

--- spec/features/new_job_spec.rb
+++ spec/features/new_job_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
-feature "Advertising a job" do
+feature "Creating a job" do
   let(:body)  { Faker::Lorem.paragraphs }
   let(:title) { Faker::Lorem.sentence   }
   let(:user)  { Fabricate(:user)        }
 
   scenario "displays a success message w/ valid input" do
     visit user_home_page_path(user)
-    click_button("Create Job Ad")
+    click_button("New Job")
     fill_in "job_ad_title", with: title
     fill_in "job_ad_body",  with: body
     click_button("Submit")
@@ -18,7 +18,7 @@ feature "Advertising a job" do
 
   scenario "displays error messages w/ invalid input" do
     visit user_home_page_path(user)
-    click_button("Create Job Ad")
+    click_button("New Job")
     click_button("Submit")
     expect(page.body).to have_content("Title can't be blank")
     expect(page.body).to have_content("Body can't be blank")
