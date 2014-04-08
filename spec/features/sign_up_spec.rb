28695cf89247dd205db7e9f5ca0cd5fa6ce01082
Michael Pope <amorphid@captaincoder.io>
2014-04-08 14:57:48 -0700

    Refactor sign up feature tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,7 +3,10 @@ require "spec_helper"
 feature "Signing up" do
   given(:email) { Faker::Internet.email }
 
-  background { visit new_sign_up_email_path }
+  background do
+    visit root_path
+    click_link("Sign up")
+  end
 
   scenario "displays message w/ valid input" do
     fill_in "sign_up_email", with: email
