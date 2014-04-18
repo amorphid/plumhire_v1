0a29e1ac16f6c7f550e726489755905d51d2d46c
Michael Pope <amorphid@captaincoder.io>
2014-04-17 20:10:50 -0700

    (red) Add failing routing test => JobsController should route GET /jobs/1 to/from {:controller=>"jobs", :id=>"1", :action=>"show"}

--- spec/routing/jobs_controller_spec.rb
+++ spec/routing/jobs_controller_spec.rb
@@ -2,6 +2,14 @@ require "spec_helper"
 
 describe JobsController do
   it do
+    should route(:get, "/jobs/1").to(
+      controller: "jobs",
+      id:         "1",
+      action:     "show"
+    )
+  end
+
+  it do
     should route(:get, "/jobs/new").to(
       controller: "jobs",
       action:     "new"
