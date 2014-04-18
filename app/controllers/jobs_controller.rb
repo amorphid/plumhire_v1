db5965f073e7193ee0f6744d57fbe4888ea4eba2
Michael Pope <amorphid@captaincoder.io>
2014-04-17 18:35:59 -0700

    (green) Make controller test pass => JobsController#update sets @j

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,5 +4,14 @@ class JobsController < ApplicationController
   end
 
   def update
+    @j = Job.find_or_initialize_by(uuid: params[:id])
+    @j.update(job_params)
+    render action: "new"
+  end
+
+  private
+
+  def job_params
+    params[:job].permit(:title, :body)
   end
 end
