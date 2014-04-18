d8f11e552e570277d8c18f42841f1544b6a6e99b
Michael Pope <amorphid@captaincoder.io>
2014-04-17 21:26:57 -0700

    (red) Write failing controller test => UserHomePagesController#show redirect if not signed in

--- spec/controllers/user_home_pages_controller_spec.rb
+++ spec/controllers/user_home_pages_controller_spec.rb
@@ -2,13 +2,22 @@ require "spec_helper"
 
 describe UserHomePagesController do
   context "#show" do
-    it "sets @user" do
+    it "sets @user if signed in" do
+      # u = Fabricate(:user)
+      # get(
+      #   :show,
+      #   id: u.uuid
+      # )
+      # expect(assigns[:user]).to be_instance_of(User)
+    end
+
+    it "redirect if not signed in" do
       u = Fabricate(:user)
       get(
         :show,
         id: u.uuid
       )
-      expect(assigns[:user]).to be_instance_of(User)
+      expect(response).to redirect_to(sign_ins_path)
     end
   end
 end
