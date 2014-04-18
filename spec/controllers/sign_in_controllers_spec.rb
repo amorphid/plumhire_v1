d95c3924e2dac31d60dbebd8ee4a7e31d283ecfa
Michael Pope <amorphid@captaincoder.io>
2014-04-18 08:59:51 -0700

    (red) Write failing controller test => SignInsController#update sets @u

--- spec/controllers/sign_in_controllers_spec.rb
+++ spec/controllers/sign_in_controllers_spec.rb
@@ -3,7 +3,19 @@ require "spec_helper"
 describe SignInsController do
   context "#new" do
     it "sets @u" do
-      get :new
+      get(:new)
+      expect(assigns[:u]).to be_instance_of(User)
+    end
+  end
+
+  context "#update" do
+    it "sets @u" do
+      u = Fabricate(:user)
+      put(
+        :update,
+        user: u.attributes,
+        id:   u.uuid
+      )
       expect(assigns[:u]).to be_instance_of(User)
     end
   end
