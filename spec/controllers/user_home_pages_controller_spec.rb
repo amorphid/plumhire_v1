5dea14fb949bfa4022e4006cb5555765c52339da
Michael Pope <amorphid@captaincoder.io>
2014-04-19 21:39:49 -0700

    Refactor UserHomePagesController tests

--- spec/controllers/user_home_pages_controller_spec.rb
+++ spec/controllers/user_home_pages_controller_spec.rb
@@ -1,24 +1,25 @@
 require "spec_helper"
 
 describe UserHomePagesController do
+  let(:get_show) do
+    get(
+      :show,
+      id: u.uuid
+    )
+  end
+
+  let(:u) { Fabricate(:user) }
+
+  before { session[:user_id] = u.id }
+
   context "#show" do
     it "sets @user if signed in" do
-      u = Fabricate(:user)
-      session[:user_id] = u.id
-      get(
-        :show,
-        id: u.uuid
-      )
+      get_show
       expect(assigns[:user]).to be_instance_of(User)
     end
 
-    it "redirects if not signed in" do
-      u = Fabricate(:user)
-      get(
-        :show,
-        id: u.uuid
-      )
-      expect(response).to redirect_to(new_sign_in_path)
+    it_behaves_like "require_sign_in" do
+      let(:action) { get_show }
     end
   end
 end
