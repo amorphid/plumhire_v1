21e4fd675851962818f4f73bd64de7e1d5bbb30e
Michael Pope <amorphid@captaincoder.io>
2014-04-19 20:54:08 -0700

    Refactor JobsController tests

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -4,72 +4,70 @@ describe JobsController do
   before { sign_in }
 
   context "#new" do
+    let(:get_new) { get :new }
+
     it "sets @j" do
-      get :new
+      get_new
       expect(assigns[:j]).to be_instance_of(Job)
     end
 
     it_behaves_like "require_sign_in" do
-      let(:action) { get :new }
+      let(:action) { get_new }
     end
   end
 
   context "#show" do
-    let(:j) { Fabricate(:job) }
-
-    it "sets @j" do
+    let(:get_show) do
       get(
         :show,
-        job: j.attributes,
-        id:  j.uuid
+        job: job.attributes,
+        id:  job.uuid
       )
-      expect(assigns[:j]).to eq(j)
+    end
+
+    let(:job) { Fabricate(:job) }
+
+    it "sets @j" do
+      get_show
+      expect(assigns[:j]).to eq(job)
+    end
+
+    it_behaves_like "require_sign_in" do
+      let(:action) { get_show }
     end
   end
 
   context "#update" do
     let(:j) { Fabricate.build(:job) }
 
-    it "sets @j" do
+    let(:put_update) do
       put(
         :update,
         job: j.attributes,
         id:  j.uuid
       )
+    end
+
+    it "sets @j" do
+      put_update
       expect(assigns[:j]).to be_instance_of(Job)
     end
 
     it "redirect" do
-      put(
-        :update,
-        job: j.attributes,
-        id:  j.uuid
-      )
+      put_update
       expect(response).to redirect_to(job_path(j))
     end
 
     it "creates 1 job w/ 1 put request" do
       count = Job.count
-      put(
-        :update,
-        job: j.attributes,
-        id:  j.uuid
-      )
+      put_update
       expect(Job.count).to eq(count + 1)
     end
 
     it "creates 1 job w/ 2 put requests" do
       count = Job.count
-      put(
-        :update,
-        job: j.attributes,
-        id:  j.uuid
-      )
-      put(
-        :update,
-        job: j.attributes,
-        id:  j.uuid
-      )
+      put_update
+      put_update
       expect(Job.count).to eq(count + 1)
     end
 
