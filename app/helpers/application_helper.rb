09f49dbe0691a4631507fedce389ba90ef326ad2
Michael Pope <amorphid@captaincoder.io>
2014-04-21 13:17:05 -0700

    Add mobile_browser? method to ApplicationController

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def mobile_browser?
+    browser    = request.env["HTTP_USER_AGENT"]
+    user_agent = UserAgent.parse(browser)
+    user_agent.mobile?
+  end
+
   def page_title(title)
     provide(:title, title)
   end
